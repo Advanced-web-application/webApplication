551aac415fe9026b2d29526f9d9776f89af37dbb
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2024-01-16 12:58:43 +0200

    fixed auth test

--- dist/src/tests/auth.test.js
+++ dist/src/tests/auth.test.js
@@ -15,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
 const app_1 = __importDefault(require("../app"));
 const mongoose_1 = __importDefault(require("mongoose"));
-const Customer_model_1 = __importDefault(require("../models/Customer_model"));
+const user_model_1 = __importDefault(require("../models/user_model"));
 let app;
 const user = {
     email: "testUser@test.com",
@@ -24,7 +24,7 @@ const user = {
 beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
     app = yield (0, app_1.default)();
     console.log("beforeAll");
-    yield Customer_model_1.default.deleteMany({ 'email': user.email });
+    yield user_model_1.default.deleteMany({ 'email': user.email });
 }));
 afterAll(() => __awaiter(void 0, void 0, void 0, function* () {
     yield mongoose_1.default.connection.close();
@@ -61,18 +61,18 @@ describe("Auth tests", () => {
         expect(accessToken).toBeDefined();
     }));
     test("Test forbidden access without token", () => __awaiter(void 0, void 0, void 0, function* () {
-        const response = yield (0, supertest_1.default)(app).get("/student");
+        const response = yield (0, supertest_1.default)(app).get("/customer");
         expect(response.statusCode).toBe(401);
     }));
     test("Test access with valid token", () => __awaiter(void 0, void 0, void 0, function* () {
         const response = yield (0, supertest_1.default)(app)
-            .get("/student")
+            .get("/customer")
             .set("Authorization", "JWT " + accessToken);
         expect(response.statusCode).toBe(200);
     }));
     test("Test access with invalid token", () => __awaiter(void 0, void 0, void 0, function* () {
         const response = yield (0, supertest_1.default)(app)
-            .get("/student")
+            .get("/customer")
             .set("Authorization", "JWT 1" + accessToken);
         expect(response.statusCode).toBe(401);
     }));
@@ -80,7 +80,7 @@ describe("Auth tests", () => {
     test("Test access after timeout of token", () => __awaiter(void 0, void 0, void 0, function* () {
         yield new Promise(resolve => setTimeout(() => resolve("done"), 5000));
         const response = yield (0, supertest_1.default)(app)
-            .get("/student")
+            .get("/customer")
             .set("Authorization", "JWT " + accessToken);
         expect(response.statusCode).not.toBe(200);
     }));
@@ -95,7 +95,7 @@ describe("Auth tests", () => {
         const newAccessToken = response.body.accessToken;
         newRefreshToken = response.body.refreshToken;
         const response2 = yield (0, supertest_1.default)(app)
-            .get("/student")
+            .get("/customer")
             .set("Authorization", "JWT " + newAccessToken);
         expect(response2.statusCode).toBe(200);
     }));
