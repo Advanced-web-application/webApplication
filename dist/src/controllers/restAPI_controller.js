6c2920bce7b84fabe386c6f343efd9806f82d3d5
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2024-01-22 18:12:05 +0200

    part 3 - succeeded♥♥♥

---
+++ dist/src/controllers/restAPI_controller.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+class RestAPIController {
+    // Add the getCurrencyRate method inside the class
+    getCurrencyRate() {
+        const options = {
+            method: 'GET',
+            url: 'https://v6.exchangerate-api.com/v6/7dd05392ee9911b244b282d8/latest/USD',
+            params: { category: 'all', count: '10' },
+            headers: {
+                'X-RapidAPI-Key': '7dd05392ee9911b244b282d8',
+                'X-RapidAPI-Host': 'v6.exchangerate-api.com',
+            },
+        };
+        axios_1.default
+            .request(options)
+            .then(function ({ data }) {
+            console.log(data);
+        })
+            .catch(function (error) {
+            console.error(error);
+        });
+    }
+}
+exports.default = new RestAPIController();
+//# sourceMappingURL=restAPI_controller.js.map
--- src/routes/restAPI_route.ts
+++ src/routes/restAPI_route.ts
@@ -2,5 +2,5 @@ import express from "express";
 const router = express.Router();
 import restAPIController from "../controllers/restAPI_controller";
 
-router.get("/", restAPIController.get.bind(restAPIController));
+router.get("/", restAPIController.getCurrencyRate.bind(restAPIController));
 export default router;
