8d5676d7a35f8452810f8e3321922a1660c8bfdd
Michal Massachi <michal2792000@gmail.com>
2024-02-16 20:57:17 +0200

    outsideAPI

--- src/controllers/restAPI_controller.ts
+++ src/controllers/restAPI_controller.ts
@@ -1,15 +1,40 @@
 import axios from 'axios';
+import { Request, Response } from 'express';
 
 // Move the interface outside the class
-interface Response {
-  result: string;
-  baseCode: string;
-  conversion_rates: Map<string, number>;
-}
+// interface Response {
+//   result: string;
+//   baseCode: string;
+//   conversion_rates: Map<string, number>;
+// }
+
+// class RestAPIController {
+//   // Add the getCurrencyRate method inside the class
+//   getCurrencyRate() {
+//     const options = {
+//       method: 'GET',
+//       url: 'https://v6.exchangerate-api.com/v6/7dd05392ee9911b244b282d8/latest/USD',
+//       params: { category: 'all', count: '10' },
+//       headers: {
+//         'X-RapidAPI-Key': '7dd05392ee9911b244b282d8',
+//         'X-RapidAPI-Host': 'v6.exchangerate-api.com',
+//       },
+//     };
+//     axios
+//       .request(options)
+//       .then(function ({ data }: { data: Response }) {
+//         console.log(data);
+
+//       })
+//       .catch(function (error: string) {
+//         console.error(error);
+//       });
+//   }
+// }
 
 class RestAPIController {
   // Add the getCurrencyRate method inside the class
-  getCurrencyRate() {
+  async getCurrencyRate(req: Request, res: Response) {
     const options = {
       method: 'GET',
       url: 'https://v6.exchangerate-api.com/v6/7dd05392ee9911b244b282d8/latest/USD',
@@ -19,14 +44,13 @@ class RestAPIController {
         'X-RapidAPI-Host': 'v6.exchangerate-api.com',
       },
     };
-    axios
-      .request(options)
-      .then(function ({ data }: { data: Response }) {
-        console.log(data);
-      })
-      .catch(function (error: string) {
-        console.error(error);
-      });
+    try {
+      const { data } = await axios.request(options);
+      res.send(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching currency rates');
+    }
   }
 }
 
