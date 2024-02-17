6936ed019418e134ab0a7bc9f89604939b15bc74
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2024-02-17 13:13:24 +0200

    additions for filtering posts by user

--- src/routes/post_route.ts
+++ src/routes/post_route.ts
@@ -240,7 +240,8 @@ router.delete("/:id", authMiddleware, PostController.deleteById.bind(PostControl
 
 router.put("/comment/:id",  PostController.addComment.bind(PostController)); // need to add authMiddleware
 
-
+//post route to get posts by user
+router.get('/posts', PostController.getPostsByUser);
 
 
 
